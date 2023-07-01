19526bc37cf7412bc7de00238816c87c686f4d4a
SmasherK <ksikalan@gmail.com>
2023-07-01 14:01:46 +0200

    done

--- src/components/NavbarComp.vue
+++ src/components/NavbarComp.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="container-fluid">
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
+    <div class="navigation ">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary p-0 ">
+            <div class="container-fluid  p-2" id="navBar">
               
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
-              <div class="collapse navbar-collapse" id="navbarNav">
+              <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
                 <ul class="navbar-nav">
                   <li class="nav-item">
-                    <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+                    <router-link class="nav-link " aria-current="page" to="/">Home</router-link>
                   </li>
                   <li class="nav-item">
                     <router-link class="nav-link" to="/about">About</router-link>
@@ -40,5 +40,23 @@
 </script>
 
 <style scoped>
+#navBar{
+    background-color: #06283d;
+    opacity: .75;
+}
+.nav-link{
+    color: #f3f2e9;
+    
+   
+}
+.nav-link:hover{
+    background-color: #f3f2e9;
+    border-radius: .5rem;
+    transform: translateY(0.5rem);
+    transition: top 0.5s, opacity 0.5s ;
+}
+.navbar-toggler,.navbar-toggler-icon{
+    background-color: #f3f2e9;
+}
 
 </style>
