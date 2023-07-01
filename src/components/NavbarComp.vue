a754ab14e555edc70869dd831e883d5078975f49
SmasherK <ksikalan@gmail.com>
2023-07-01 11:45:16 +0200

    folder structure done

---
+++ src/components/NavbarComp.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+              
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link class="nav-link" to="/about">About</router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link class="nav-link" to="/resume">Resume</router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link class="nav-link" to="/projects">Projects</router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link class="nav-link" to="/testimonials">Testimonials</router-link>
+                  </li>
+                  <li class="nav-item">
+                    <router-link class="nav-link" to="/contact">Contact</router-link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
