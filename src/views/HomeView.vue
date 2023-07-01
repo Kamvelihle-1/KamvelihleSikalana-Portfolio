20a25bcc20d16e2f5683d34b32bfa4c95a7b4bb6
SmasherK <ksikalan@gmail.com>
2023-07-01 14:47:35 +0200

    not done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,33 @@
 <template>
-  <div class="home">
-    
+  <div class="home " >
+    <div class="container mt-5 pt-5">
+      <h1 class="display-1">Welcome to my portfolio</h1>
+      <br>
+      <p class="display-6"><span>Hi</span>, I am Kamvelihle Sikalana an aspiring <span v-for="item in tEffect" :key="item"> {{item}}</span>, so what are we building? </p>
+    </div>
   </div>
 </template>
 
-<script>
-
-
+<script >
 
 export default {
   name: 'HomeView',
-  components: {
-   
-  }
+ 
+  data() {
+    return{
+     tEffect : ["Software Engineer","Penetration Tester","IT Security Officer"]
+      }
+    }
+  
 }
 </script>
+
+
+<style scoped>
+h1{
+  color: white;
+}
+
+</style>
+
+
