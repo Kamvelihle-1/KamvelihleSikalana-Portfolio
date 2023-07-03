1d465cf687bd33dbce3f5c143d4e385922df7667
SmasherK <ksikalan@gmail.com>
2023-07-03 11:24:34 +0200

    done

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,15 +1,72 @@
 <template>
-    <div>
+    <div class="container ">
+        <div class="row my-3">
+            <div class="col-12 ">
+                <h1 class="dispaly-1">Projects</h1>
+            </div>
+        </div>
+
+        <div class="row ">
+            <div class=" card  m-2 text-center p-0" v-for="proj in projects" :key="proj.title" >
+                <div class="row g-0">
+                  <div class="col ">
+                    <img :src="proj.image" class="img-fluid rounded-start" :alt="proj.title">
+                  </div>
+                </div>
+                <div class="row body ">
+                    <div class="col ">
+                        <div class="card-body pt-2">
+                          <h5 class="card-title fw-bold fs-large ">{{proj.title}}</h5>
+                          <br>
+                          <p class="card-text">{{proj.description}}</p>
+                          <button class="card-text m-2 p-1"><a :href="proj.website">Go To Site</a></button>
+                          <button class="card-text p-1"><a :href="proj.repoLink">Go To Repository</a></button>
+                        </div>
+                      </div>
+                </div>
+            </div>
+        </div>
 
     </div>
 </template>
 
 <script>
     export default {
-        
+        computed:{
+            projects(){
+                return  this.$store.state.projects
+            }
+        },
+        mounted(){
+            this.$store.dispatch('getProjects')
+        }
     }
 </script>
 
 <style scoped>
+.card{
+    background-color: #f3f2e9;
+    max-width: 400px;
+}
+.card-body button{
+    border-radius: .5rem;
+    background-color: #06283d; 
+   
+}
+.card-body button a{
+    text-decoration: none;
+    color: #f3f2e9;
+}
+h1{
+    color: #f3f2e9; 
+}
+.card img{
+  height: 223px;
+  max-width: 395px;
+}
+
+.projects{
+    border-radius: 1.5rem;
+}
 
 </style>
